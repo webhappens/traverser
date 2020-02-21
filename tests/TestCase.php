7f72df7b785668d30725e7dc85cf5137fe4d5cfb
Ben Gurney <ben@webhappens.co.uk>
2020-02-21 12:09:24 +0000

    Scaffold for tests

---
+++ tests/TestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WebHappens\Traverser\Tests;
+
+use PHPUnit\Framework\TestCase as PHPUnit;
+
+abstract class TestCase extends PHPUnit
+{
+}
---
+++ tests/TraverserTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace WebHappens\Traverser\Tests;
+
+class TraverserTest extends TestCase
+{
+    public function test_demo()
+    {
+        $this->assertTrue(true);
+    }
+}
